4410bf03144a53c100144b6e7b1a5068176262e7
Jing Pei <jingpei@sas.upenn.edu>
2013-01-07 23:56:36 -0700

    Questions 1-11 Completed

--- main.js
+++ main.js
@@ -137,7 +137,6 @@ $('#q6').click(function() {
 	var sumsq = 0;
 	var sqsum = 0; //sumsq = sum of squares, sqsum = square of sums
 	var difference;
-	var square=0;
 	function sqDiff(){
 		for(var i=1; i<=100; i++){
 			sumsq = sumsq+(i*i);
@@ -190,6 +189,129 @@ $('#q8').click(function() {
 		console.log("Largest " + largest);
 	}
 	largeProduct();
+});
+
+$('#q9').click(function() {
+	function isTriple(x,y,z){
+		if((z*z)+(y*y)==(x*x)){
+			return true;
+		}
+		return false;
+	}
+	function sumThousand(){
+		for(var i=1; i<1000; i++){ 
+			for(var j=1; j<i; j++){ 
+				for(var k=1; k<j; k++){ 
+					if((i+j+k)==1000){
+						if(isTriple(i,j,k)){
+							var product = i * j * k;
+							console.log("a = " + k + " b = " + j + " c = " + i);
+							console.log("Product " + product);
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+	sumThousand();
+});
+
+$('#q10').click(function() {
+	var sum = 0;
+	function isPrime(args){
+		for(var i=2; i<=Math.sqrt(args); i++){
+			if(args%i==0){
+				return false;
+			}
+		}
+		return true;
+	}
+	function primeSum(){
+		for(var i=2; i<2000000; i++){
+			if(isPrime(i)){
+				sum = sum + i;
+			}
+		}
+		console.log("Sum " + sum);
+	}
+	primeSum();
+});
+
+$('#q11').click(function() {
+	var grid = 
+	[[08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08],[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00],[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65],[52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91],[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],[24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],[67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21],[24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],[21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95],[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92],[16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57],[86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],[19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40],[04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],[88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],[04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36],[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16],[20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54],[01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48]];
+
+	var largest = 0;
+	function upDown(args){
+		for(var i=0; i<args[0].length; i++){
+			for(var j=0; j<args.length-3; j++){
+				var product = 1;
+				for(var k=i; k<i+4; k++){
+					product = product*args[i][k];
+					if(j==i+3 && largest<product){
+						largest = product;
+					}
+				}
+			}
+		}
+		return largest;
+	}
+	
+	function rightLeft(args){
+		for(var i=0; i<args.length; i++){
+			for(var j=0; j<args[i].length-3; j++){
+				var product = 1;
+				for(var k=j; k<j+4; k++){
+					product = product*args[k][i];
+					if(j==i+3 && largest<product){
+						largest = product;
+					}
+				}
+			}
+		}
+	}
+	
+	function diagonal(args){ 
+		var productrt = 1;
+		var productlt = 1; 
+		for(var i=0; i<args.length-3; i++){ //rows (y)
+			for(var j=0; j<args[i].length; j++){ //columns (x)
+				if(j-3 >= 0){
+					productlt = args[i][j];
+					var n = 3;
+					while(n > 0){
+						productlt = productlt * args[i+n][j-n];
+						n--;
+					}
+				}
+				if(j+3 < 20){
+					productrt = args[i][j];
+					var m = 3;
+					while(m > 0){
+						productrt = productrt * args[i+m][j+m];
+						m--;
+					}
+				}
+				if(largest < productlt || largest < productrt){
+					if(productlt > productrt){
+						largest = productlt;
+					}
+					else{
+						largest = productrt;
+					}
+				}
+			}
+		}
+	}
+	
+	function largestProd(){
+		var x = upDown(grid);
+		var y = rightLeft(grid);
+		var z = diagonal(grid);
+		console.log("Largest " + largest);
+	}
+	largestProd(grid);
 });
 
 // THIS IS THE BOTTOM OF YOUR PROGRAM
